fe0c5adf273abdc9b587e687615375065942e94a
337yj <337yje@gmail.com>
2023-05-15 08:16:31 +0900

    test : Automate AWS deployment

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,6 +5,9 @@ import whiteLogo from "../../assets/images/whiteLogo.png";
 import mainImage from "../../assets/images/mainImg.jpg";
 import mainImage2 from "../../assets/images/mainImg2.jpg";
 import mainImage3 from "../../assets/images/mainImg3.jpg";
+import product1 from "../../assets/images/product1.jpg";
+import product2 from "../../assets/images/product2.jpg";
+import product3 from "../../assets/images/product3.jpg";
 import styles from "./home.module.scss";
 
 const Home = () => {
@@ -51,6 +54,11 @@ const Home = () => {
             In Order To Be Irreplaceable One Must Always Be Different
           </p>
           <img src={whiteLogo} className={styles.logo} alt="Logo" />
+        </div>
+        <div className={styles.productWrap}>
+          <img src={product1} className={styles.product1} alt="product" />
+          <img src={product2} className={styles.product2} alt="product" />
+          <img src={product3} className={styles.product3} alt="product" />
         </div>
       </section>
 
